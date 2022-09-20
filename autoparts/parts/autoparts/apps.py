141eb141139d2b267db54cafb2eefb612137cc43
Nikolay <nikolaygv@mail.ru>
2022-09-20 23:20:26 +0300

    final-work

--- autoparts/parts/autoparts/apps.py
+++ autoparts/parts/autoparts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PartsConfig(AppConfig):
+class AutopartsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'parts'
+    name = 'autoparts'
